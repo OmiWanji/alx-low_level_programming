3db80195610ba37ab4a469b3d01b7361c7b64429
NAOMI-WANJIRU <shiroomi6@gmail.com>
2022-12-15 19:13:43 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * _isupper - function that checks for uppercase character
- * @c:Returns 1 if c is uppercase;
- * Returns: 0 otherwise
+ * @c: inputs characters
+ * Returns: 1 if upercase 0 otherwise
  */
-int isupper(int c);
+int _isupper(int c);
 {
 	if (c >= 65 && c <= 90)
 	{
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if number are 0 - 9
+ * @c: inputs character
+ * Return: 1 if c is a digit and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+} 
