fb756aa978ff36183828a9a1b37935b1f6c80552
NAOMI-WANJIRU <shiroomi6@gmail.com>
2022-12-15 18:00:27 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c:Returns 1 if c is uppercase;
+ * Returns: 0 otherwise
+ */
+int isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+ }
