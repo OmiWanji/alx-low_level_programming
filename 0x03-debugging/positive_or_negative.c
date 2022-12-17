ae93fc99a49d77b02b6ac8295a1d5bc52a15778c
NAOMI-WANJIRU <shiroomi6@gmail.com>
2022-12-16 16:16:51 -0800

    1-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>                                          
+
+/**                                           
+ * positive_or_negative - test function that is positive or negative                                                    
+ * @i: The int to be tested
+ * Return: on success 0                                      
+ * On error , 1 is returned, and error is set appropriately 
+ */                                                          
+
+void positive_or_negative(int i)                                     
+{                                                            
+
+	        return (write(+ &i, -));                            
+
+}                      
