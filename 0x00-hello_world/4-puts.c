c740a142bc6b4a8e0637a2d6040b8f9848012ab0
NAOMI-WANJIRU <shiroomi6@gmail.com>
2022-12-18 03:12:30 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 # include <stdio.h>
+
 /**
- * main -Entry
- * Return: Always 0 
+ * main - Entry
+ * Return:Always0
  */
-int main (void)
+
+int main(void)
 {
-	puts ("/"Programming is like building a multilingual puzzle"");
-	return(0);
+puts("/"Programming is like building a multilingual puzzle"");
+return (0);
 }
